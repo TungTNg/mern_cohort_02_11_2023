1d908e1af8b35338eefdc469d0ef5742ef3f7567
Tung Nguyen <tnguyen7@fredhutch.org>
2023-02-13 19:30:11 -0800

    Add checkbox to todo app

--- app.js
+++ app.js
@@ -1,70 +1,69 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-  let todoItemsArr = [
-    {
-      id: self.crypto.randomUUID(),
-      title: 'Go to the store',
-      completed: false
-    },
-    {
-      id: self.crypto.randomUUID(),
-      title: 'Go to the mall',
-      completed: false
-    },
-    {
-      id: self.crypto.randomUUID(),
-      title: 'Go to the park',
-      completed: true
-    }
-  ]
+  fetch('https://jsonplaceholder.typicode.com/todos/')
+    .then(response => response.json())
+    .then(todoItemsArr => {
+      const todoInputElement = document.querySelector('#todoInput')
+      const todoAddButtonElement = document.querySelector('#addTodoButton')
+      const todoListElement = document.querySelector('#todoList')
+    
+      const renderTodoItems = () => {
+        console.log('rendering todo items....')
+    
+        todoListElement.innerHTML = ''
+    
+        todoItemsArr.forEach((todoItem)=>{
+          const todoItemElement = document.createElement('li')
+          todoItemElement.innerText = todoItem.title
+          todoItemElement.setAttribute('data-todo-id', todoItem.id)
+          todoItem.completed == true ? todoItemElement.classList.add('completed-task') : todoItemElement.classList.remove('completed-task')
+          todoListElement.appendChild(todoItemElement)
+          
+          const completedCheckBox = document.createElement('input')
+          completedCheckBox.setAttribute('type', 'checkbox')
+          completedCheckBox.setAttribute('data-todo-id', todoItem.id)
+          completedCheckBox.checked = todoItem.completed
+          todoItemElement.appendChild(completedCheckBox)
 
-  const todoInputElement = document.querySelector('#todoInput')
-  const todoAddButtonElement = document.querySelector('#addTodoButton')
-  const todoListElement = document.querySelector('#todoList')
+          completedCheckBox.addEventListener('change', ()=>{
+            const completedTodoId = completedCheckBox.getAttribute('data-todo-id')
+            const completedTodoItem = todoItemsArr.find((todoItem)=>{
+              return todoItem.id == completedTodoId
+            })
+            completedTodoItem.completed = completedCheckBox.checked
+            renderTodoItems()
+          })
 
-  const renderTodoItems = () => {
-    console.log('rendering todo items....')
-
-    todoListElement.innerHTML = ''
-
-    todoItemsArr.forEach((todoItem)=>{
-      const todoItemElement = document.createElement('li')
-      todoItemElement.innerText = todoItem.title
-      todoItemElement.setAttribute('data-todo-id', todoItem.id)
-      todoListElement.appendChild(todoItemElement)
-
-
-      const deleteTodoButtonElement = document.createElement('button')
-      deleteTodoButtonElement.innerText = 'Delete'
-      deleteTodoButtonElement.setAttribute('data-todo-id', todoItem.id)
-      todoItemElement.appendChild(deleteTodoButtonElement)
-
-      deleteTodoButtonElement.addEventListener('click', ()=>{
-        const deleteTodoId = deleteTodoButtonElement.getAttribute('data-todo-id')
-
-        todoItemsArr = todoItemsArr.filter((todoItem) => {
-          return todoItem.id !== deleteTodoId;
+          const deleteTodoButtonElement = document.createElement('button')
+          deleteTodoButtonElement.innerText = 'Delete'
+          deleteTodoButtonElement.setAttribute('data-todo-id', todoItem.id)
+          todoItemElement.appendChild(deleteTodoButtonElement)
+    
+          deleteTodoButtonElement.addEventListener('click', ()=>{
+            const deleteTodoId = deleteTodoButtonElement.getAttribute('data-todo-id')
+    
+            todoItemsArr = todoItemsArr.filter((todoItem) => {
+              return todoItem.id != deleteTodoId;
+            })
+            
+            renderTodoItems()
+          })
         })
-        
+      }
+    
+      renderTodoItems()
+    
+      todoAddButtonElement.addEventListener('click', ()=>{
+        const newTodoItem = {
+          id: crypto.randomUUID(),
+          title: todoInputElement.value,
+          completed: false
+        }
+    
+        todoItemsArr.push(newTodoItem)
+        todoInputElement.value = ''
         renderTodoItems()
-      })
-    })
-  }
-
-  renderTodoItems()
-
-  todoAddButtonElement.addEventListener('click', ()=>{
-    const newTodoItem = {
-      id: self.crypto.randomUUID(),
-      title: todoInputElement.value,
-      completed: false
-    }
-
-    todoItemsArr.push(newTodoItem)
-    todoInputElement.value = ''
-    renderTodoItems()
-  })
-  
-
+      })    
+    })  
 })
 
 
